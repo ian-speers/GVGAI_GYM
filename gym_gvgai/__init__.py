22aec1f765bc8ec5b3c91f8e64dcc45c57db0720
Ian Speers <ianmspeers@gmail.com>
2025-05-26 14:00:11 -0400

    ...

--- gym_gvgai/__init__.py
+++ gym_gvgai/__init__.py
@@ -10,17 +10,19 @@ for game in games:
 	gamePath = os.path.join(gamesPath, game)
 	if 'sokoban' in game:
 		print('\n\n\n\n\n', 'for games', game)
-		print('\n\n\n\n\n', 'for gamePath', gamePath)
+		print('for gamePath', gamePath)
 	if(os.path.isdir(gamePath)):
 		#Currently if there are more than 5 levels, JavaServer.java will not load them. It expects lvl0 - lvl4.
 		lvls = len([lvl for lvl in os.listdir(gamePath) if 'lvl' in lvl])
 		if 'sokoban' in game:
-			print('\n\n\n\n\n', 'lvls', lvls)
+			print('lvls', lvls)
 		for lvl in range(lvls):
 			#    for obs_type in ['image', 'json']:
 			# space_invaders should yield SpaceInvaders-v0 and SpaceInvaders-ram-v0
 			name = game.split('_')[0]
 			version = int(game.split('_')[-1][1:])
+			if 'sokoban' in game:
+				print('register', 'name', name, 'version', version)
 			register(
 	    		id='gvgai-{}-lvl{}-v{}'.format(name, lvl, version),
 	    		entry_point='gym_gvgai.envs.gvgai_env:GVGAI_Env',
